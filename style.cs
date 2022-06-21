da50f7b2236415222cea462727fc947064c2ad9a
B-Eulalia <101184278+B-Eulalia@users.noreply.github.com>
2022-06-21 14:07:31 +0300

    Create style.cs

---
+++ style.cs
@@ -0,0 +1,24 @@
+h1 {
+text-align: center;
+}
+
+table {
+border: 1px solid black;
+background-color: grey;
+}
+
+td {
+border: 1px solid black;
+padding: 20px 20px 20px;
+background-color: grey;
+}
+
+.F3CC {
+color: red;
+}
+
+#reset {
+border: 1px solid black;
+padding: 15px 25px 15px;
+background-color: grey;
+}
